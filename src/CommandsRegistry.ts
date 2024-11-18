a8f664e3c6a22a4083a664ddc7f835245574994a
skytomo <tomo1kawa.fullerene@gmail.com>
2024-11-18 23:56:47 +0900

    feat: #40 add safe level to CommandsRegistry

--- src/CommandsRegistry.ts
+++ src/CommandsRegistry.ts
@@ -1,14 +1,39 @@
+type Callback = (...props: any[]) => any;
+const SAFETY_LEVELS = ['read', 'load', 'dengerous'] as const;
+type SafetyLevel = (typeof SAFETY_LEVELS)[number];
+type Value = { callback: Callback; safetyLevel: SafetyLevel };
 export default class CommandsRegistry {
-  protected readonly commands: Map<string, (...props: any[]) => any> =
-    new Map();
+  protected readonly commands: Map<string, Value> = new Map();
 
-  public registerCommand(command: string, callback: (...props: any[]) => any) {
-    this.commands.set(command, callback);
+  public registerCommand(
+    command: string,
+    callback: Callback,
+    safetyLevel: SafetyLevel = 'read',
+  ) {
+    this.commands.set(command, { callback, safetyLevel });
   }
 
   public executeCommand(command: string, ...props: any[]) {
-    const callback = this.commands.get(command);
-    if (!callback) return undefined;
+    const value = this.commands.get(command);
+    if (!value) return undefined;
+    const { callback } = value;
+    return callback(...props);
+  }
+
+  public executeCommandSafely(
+    safetyLevel: SafetyLevel,
+    command: string,
+    ...props: any[]
+  ) {
+    const value = this.commands.get(command);
+    if (!value) return undefined;
+    const { callback, safetyLevel: commandSafetyLevel } = value;
+    if (
+      SAFETY_LEVELS.indexOf(commandSafetyLevel) <
+      SAFETY_LEVELS.indexOf(safetyLevel)
+    ) {
+      throw new Error(`Command ${command} is too dangerous to execute`);
+    }
     return callback(...props);
   }
 
